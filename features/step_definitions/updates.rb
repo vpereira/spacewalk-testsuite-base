a396234b83038754b6bd408e99e96701c00bfea7
Martin Seidl <mseidl@suse.de>
2014-03-14 15:52:13 +0100

    updated repo list

--- features/step_definitions/updates.rb
+++ features/step_definitions/updates.rb
@@ -17,7 +17,8 @@ When(/^I start SUSE Manager$/) do
 end
 
 When(/^I add test repos$/) do
-    sshout = `echo | ssh -l root -o StrictHostKeyChecking=no $TESTHOST zypper ar -f http://you.suse.de/download/x86_64/update/SUSE-MANAGER/1.7/ test_updates 2>&1`
+    sshout = `echo | ssh -l root -o StrictHostKeyChecking=no $TESTHOST zypper ar -f http://you.suse.de/download/x86_64/update/SUSE-MANAGER/1.7/ test_suma 2>&1`
+    sshout = `echo | ssh -l root -o StrictHostKeyChecking=no $TESTHOST zypper ar -f hhttp://you.suse.de/download/x86_64/update/SLE-SERVER/11-SP2 test_sles 2>&1`
     if ! $?.success?
         raise "Execute command failed: #{$!}: #{output}"
     end
